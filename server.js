bc99b2b1101df8a19c8591e6068946561a30ddc6
RomashkaU <romazarik4@gmail.com>
2024-01-17 17:35:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the build folder
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+// Define a route that always serves your index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
